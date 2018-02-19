a748e4e5acd55dae9b49be9dd31e4102f0704bc3
Robin.G <490158032@qq.com>
2018-02-19 22:33:31 +0800

    [kv] add put and delete method

--- src/main/java/com/robingong/client/KvClient.java
+++ src/main/java/com/robingong/client/KvClient.java
@@ -2,9 +2,7 @@ package com.robingong.client;
 
 import com.robingong.configuration.ConfigContext;
 import com.robingong.util.GsonHolder;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -21,9 +19,10 @@ public class KvClient {
 
     @PostConstruct
     public void initClient() throws MalformedURLException {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        httpClient = builder.build();
-
+        if (httpClient == null) {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            httpClient = builder.build();
+        }
     }
 
     public String getKv(String key) throws IOException {
@@ -33,8 +32,23 @@ public class KvClient {
         List<Map<String, String>> responseList = GsonHolder.instance.getGson().fromJson(response.body().string(), List.class);
         return new String(Base64.getDecoder().decode(responseList.get(0).get("Value")), "utf-8");
     }
+
+    public boolean setKv(String key, String value) throws IOException {
+        URL url = buildUrl(key);
+        RequestBody body = RequestBody.create(MediaType.parse("text/plain; charset=utf-8"), value);
+        Request request = new Request.Builder().url(url).put(body).build();
+        Response response = httpClient.newCall(request).execute();
+        return Boolean.parseBoolean(response.body().string().trim());
+    }
+
+    public boolean deleteKv(String key) throws IOException {
+        URL url = buildUrl(key);
+        Request request = new Request.Builder().url(url).delete().build();
+        Response response = httpClient.newCall(request).execute();
+        return Boolean.parseBoolean(response.body().string().trim());
+    }
+
     private URL buildUrl(String key) throws MalformedURLException {
         return new URL(ConfigContext.getProtocol(), ConfigContext.getAddress(), ConfigContext.getPort(), BASE_PATH + key);
     }
-
 }
